4ab5209c0024a9e751b6cc77001dfbcc7ab3a266
Shona Chan <mojavealoe@gmail.com>
2023-03-07 15:13:33 +0800

    allows user to delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,12 @@ class ArticlesController < ApplicationController
     redirect_to article_path
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
