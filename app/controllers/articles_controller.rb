32e358e118aa3574ed2fbdff7ca492450c5b09db
Shona Chan <mojavealoe@gmail.com>
2023-03-07 15:07:26 +0800

    allows editing of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
-
   # GET /articles
   def index
     @articles = Article.all
   end
+
   # GET /articles/:id
   def show
     @article = Article.find(params[:id])
@@ -11,4 +11,23 @@ class ArticlesController < ApplicationController
 
   # GET /articles/new
 
+  # GET articles/:id/edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # PATCH articles/:id
+  def update
+    Article.update!(article_params)
+    redirect_to article_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(
+      :title,
+      :content
+    )
+  end
 end
